a33bc1c5b5c7423d5bf20cff919ced2a00fe2d30
hamish <hamish.tennent@gmail.com>
2022-12-15 13:41:57 +0800

    added images and swiper blocks in

---
+++ swiperscript.js
@@ -0,0 +1,31 @@
+var swiper = new Swiper(".imgSwiper", {
+    slidesPerView: 4,
+    spaceBetween: 0,
+    slidesPerGroup: 3,
+    autoplay: {
+      delay: 2000, 
+    },
+    loop: true,
+    loopFillGroupWithBlank: false,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+  });
+
+var swiper = new Swiper(".logoSwiper", {
+    slidesPerView: 5,
+    spaceBetween: 50,
+    slidesPerGroup: 3,
+    autoplay: {
+      delay: 2000, 
+    },
+    loop: true,
+    loopFillGroupWithBlank: false,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+  });
